ea6c8a14c5bedb1dae98a099cf0e0b10718fbafa
ricajust <ricartoonstudio@gmail.com>
2025-04-21 10:48:08 -0300

    feat: create currency, phone and date pipe masks

--- src/app/brokers/brokers.module.ts
+++ src/app/brokers/brokers.module.ts
@@ -2,13 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '../shared/material/material.module';
+import { MasksPipe } from '../shared/pipes/masks.pipe';
 import { BrokersRoutingModule } from './brokers-routing.module';
 import { BrokersComponent } from './brokers/brokers.component';
 
 
 @NgModule({
   declarations: [
-    BrokersComponent
+    BrokersComponent,
+    MasksPipe
   ],
   imports: [
     CommonModule,
--- src/app/shared/pipes/masks.pipe.ts
+++ src/app/shared/pipes/masks.pipe.ts
@@ -5,8 +5,46 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MasksPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, type: string): string {
+    switch (type) {
+      case "phone":
+        return this.phoneFormatter(value);
+      case "currency":
+        return this.currencyFormatter(value);
+      case "date":
+        return this.dateFormatter(value);
+
+      default:
+        console.log("Formato de máscara inválido!");
+        break;
+    }
+    return value;
   }
 
+  phoneFormatter(number: string): string {
+    number = number.replace(/\D/g, ''); // Remove caracteres não numéricos
+    if (number.length === 11) {
+      return number.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3'); // Celular
+    } else if (number.length === 10) {
+      return number.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3'); // Fixo
+    } else if (number.length === 8) {
+      return number.replace(/(\d{4})(\d{4})/, '$1-$2'); // Fixo
+    }
+    return number; // Retorna sem formatação se não corresponder
+  }
+
+  currencyFormatter(value: string): string {
+    const numericValue = parseFloat(value); // Converte para número de forma segura
+    // Formata para o padrão brasileiro
+    const formattedValue = numericValue.toLocaleString('pt-BR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    });
+    return `R$ ${formattedValue}`; // Retorna o valor formatado com o símbolo da moeda
+  }
+
+  dateFormatter(value: string): string {
+    const [year, mounth, day] = value.split("-");
+    return `${day}/${mounth}/${year}`;
+  }
 }
